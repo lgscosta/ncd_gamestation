9a9ad14aeb691dcb9b0234ee9f9e68f18ef4fa7a
Luana Costa <luanacosta@MacBook-Air-de-Luana.local>
2023-10-28 14:50:09 -0300

    More comments

--- wordle/wordle.py
+++ wordle/wordle.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 import random
-from words import *
+from words import * # Dicionário de palavras (versão inicial)
 
 pygame.init()
 
@@ -11,23 +11,26 @@ height = 850
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Wordle - NCD GameStation")
 
+# Cores 
 green = "#bcd246"
 yellow = "#f4ad42"
 grey = "#787c7e"
 outline = "#d3d6da"
 filled_outline = "#4b8fbd"
 
+# Escolha da palavra no dicionário
 correct_word = random.choice(WORDS)
-# Print da primeira escolha de palavra, já que não há verificação ainda
-print(correct_word)
+print(correct_word) # Print da primeira escolha de palavra, já que não há verificação ainda
 
-alphabet = ["QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"]
+# Definição da fonte usada no NCD
 guessed_letter_font = pygame.font.Font("Montserrat-Black.otf", 50)
 availabe_letter_font = pygame.font.Font("Montserrat-Black.otf", 25)
 
+# Inicialização da tela
 screen.fill("white")
 pygame.display.update()
 
+# Espaçamento e tamanho de letra 
 letter_x_spacing = 85
 letter_y_spacing = 12
 letter_size = 75
@@ -40,22 +43,23 @@ current_guess_string = ""
 current_letter_bg_x = 110
 game_result = ""
 
+# Objeto Letra 
 class Letter:
     def __init__(self, text, bg_position):
         # Initializes all the variables, including text, color, position, size, etc.
-        self.bg_color = "white"
-        self.text_color = "black"
-        self.bg_position = bg_position
-        self.bg_x = bg_position[0]
-        self.bg_y = bg_position[1]
-        self.bg_rect = (bg_position[0], self.bg_y, letter_size, letter_size)
-        self.text = text
-        self.text_position = (self.bg_x+36, self.bg_position[1]+34)
-        self.text_surface = guessed_letter_font.render(self.text, True, self.text_color)
-        self.text_rect = self.text_surface.get_rect(center=self.text_position)
+        self.bg_color = "white" # Cor de fundo da letra
+        self.text_color = "black" # Cor da letra
+        self.bg_position = bg_position # Posição do quadrado em relação ao fundo
+        self.bg_x = bg_position[0] # Posição x
+        self.bg_y = bg_position[1] # Posição y
+        self.bg_rect = (bg_position[0], self.bg_y, letter_size, letter_size) # Quadrado da letra
+        self.text = text # Conteúdo ou a letra em si
+        self.text_position = (self.bg_x+36, self.bg_position[1]+34) # A posição da letra
+        self.text_surface = guessed_letter_font.render(self.text, True, self.text_color) # Renderização da letra
+        self.text_rect = self.text_surface.get_rect(center=self.text_position) # Retângulo delimitador
 
+    # Função que aceita uma letra 
     def draw(self):
-        # Puts the letter and text on the screen at the desired positions.
         pygame.draw.rect(screen, self.bg_color, self.bg_rect)
         if self.bg_color == "white":
             pygame.draw.rect(screen, filled_outline, self.bg_rect, 3)
@@ -63,6 +67,7 @@ class Letter:
         screen.blit(self.text_surface, self.text_rect)
         pygame.display.update()
 
+    # Função que deleta uma letra 
     def delete(self):
         # Fills the letter's spot with the default square, emptying it.
         pygame.draw.rect(screen, "white", self.bg_rect)
@@ -70,7 +75,6 @@ class Letter:
         pygame.display.update()
 
 def check_guess(guess_to_check):
-    # Goes through each letter and checks if it should be green, yellow, or grey.
     global current_guess, current_guess_string, guesses_count, current_letter_bg_x, game_result
     game_decided = False
     for i in range(5):
