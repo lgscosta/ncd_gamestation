a523ba76daac479bb6667f82a0af8a5812a17c62
Luana Costa <luanacosta@MacBook-Air-de-Luana.local>
2023-10-28 13:27:41 -0300

    Version 1.3 - the best one

--- wordle/wordle.py
+++ wordle/wordle.py
@@ -1,101 +1,161 @@
 import pygame
+import sys
+import random
+from words import *
 
-# Inicializa o PyGame
 pygame.init()
 
-# Cria uma janela
-tela = pygame.display.set_mode((600, 600))
+# Constantes
+width = 650
+height = 850
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Wordle - NCD GameStation")
 
-# Define a cor de fundo
-cor_fundo = (200, 200, 200)
+green = "#bcd246"
+yellow = "#f4ad42"
+grey = "#787c7e"
+outline = "#d3d6da"
+filled_outline = "#4b8fbd"
 
-#  Cria o texto
-texto = pygame.font.SysFont("Arial", 20)
+correct_word = random.choice(WORDS)
+# Print da primeira escolha de palavra, já que não há verificação ainda
+print(correct_word)
 
-# Define a cor dos quadrados
-cor_rosa = (255, 105, 180)
+alphabet = ["QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"]
+guessed_letter_font = pygame.font.Font("Montserrat-Black.otf", 50)
+availabe_letter_font = pygame.font.Font("Montserrat-Black.otf", 25)
 
-# Define a largura da margem
-margem = 10
+screen.fill("white")
+pygame.display.update()
 
-# Cria uma matriz de quadrados
-quadrados = []
+letter_x_spacing = 85
+letter_y_spacing = 12
+letter_size = 75
 
-palavras = []
-for i in range(5):
-    for j in range(6):
-        quadrados.append(pygame.Rect((i * 80 + margem, j * 80 + margem), (80 - 2 * margem, 80 - 2 * margem)))
+# Variáveis Globais
+guesses_count = 0
+guesses = [[]] * 6
+current_guess = []
+current_guess_string = ""
+current_letter_bg_x = 110
+game_result = ""
 
-# Loop principal
+class Letter:
+    def __init__(self, text, bg_position):
+        # Initializes all the variables, including text, color, position, size, etc.
+        self.bg_color = "white"
+        self.text_color = "black"
+        self.bg_position = bg_position
+        self.bg_x = bg_position[0]
+        self.bg_y = bg_position[1]
+        self.bg_rect = (bg_position[0], self.bg_y, letter_size, letter_size)
+        self.text = text
+        self.text_position = (self.bg_x+36, self.bg_position[1]+34)
+        self.text_surface = guessed_letter_font.render(self.text, True, self.text_color)
+        self.text_rect = self.text_surface.get_rect(center=self.text_position)
+
+    def draw(self):
+        # Puts the letter and text on the screen at the desired positions.
+        pygame.draw.rect(screen, self.bg_color, self.bg_rect)
+        if self.bg_color == "white":
+            pygame.draw.rect(screen, filled_outline, self.bg_rect, 3)
+        self.text_surface = guessed_letter_font.render(self.text, True, self.text_color)
+        screen.blit(self.text_surface, self.text_rect)
+        pygame.display.update()
+
+    def delete(self):
+        # Fills the letter's spot with the default square, emptying it.
+        pygame.draw.rect(screen, "white", self.bg_rect)
+        pygame.draw.rect(screen, outline, self.bg_rect, 3)
+        pygame.display.update()
+
+def check_guess(guess_to_check):
+    # Goes through each letter and checks if it should be green, yellow, or grey.
+    global current_guess, current_guess_string, guesses_count, current_letter_bg_x, game_result
+    game_decided = False
+    for i in range(5):
+        lowercase_letter = guess_to_check[i].text.lower()
+        
+        guess_to_check[i].bg_color = grey
+        guess_to_check[i].text_color = "white"
+        game_result = ""
+        game_decided = True
+
+        guess_to_check[i].draw()
+        pygame.display.update()
+    
+    guesses_count += 1
+    current_guess = []
+    current_guess_string = ""
+    current_letter_bg_x = 110
+
+    if guesses_count == 6 and game_result == "":
+        game_result = "L"
+
+def play_again():
+    pygame.draw.rect(screen, "white", (10, 600, 1000, 600))
+    play_again_font = pygame.font.Font("Montserrat-Black.otf", 40)
+    play_again_text = play_again_font.render("Press ENTER to Play Again!", True, "black")
+    play_again_rect = play_again_text.get_rect(center=(width/2, 700))
+    screen.blit(play_again_text, play_again_rect)
+    pygame.display.update()
+
+def reset():
+    # Resets all global variables to their default states.
+    global guesses_count, correct_word, guesses, current_guess, current_guess_string, game_result
+    screen.fill("white")
+    guesses_count = 0
+
+    correct_word = random.choice(WORDS)
+    print(correct_word)
+    
+    guesses = [[]] * 6
+    current_guess = []
+    current_guess_string = ""
+    game_result = ""
+    pygame.display.update()
+
+def create_new_letter():
+    # Creates a new letter and adds it to the guess.
+    global current_guess_string, current_letter_bg_x
+    current_guess_string += key_pressed
+    new_letter = Letter(key_pressed, (current_letter_bg_x, guesses_count*100+letter_y_spacing))
+    current_letter_bg_x += letter_x_spacing
+    guesses[guesses_count].append(new_letter)
+    current_guess.append(new_letter)
+    for guess in guesses:
+        for letter in guess:
+            letter.draw()
+
+# precisa
+def delete_letter():
+    # Deletes the last letter from the guess.
+    global current_guess_string, current_letter_bg_x
+    guesses[guesses_count][-1].delete()
+    guesses[guesses_count].pop()
+    current_guess_string = current_guess_string[:-1]
+    current_guess.pop()
+    current_letter_bg_x -= letter_x_spacing
+
 while True:
-    # Processa os eventos
+    if game_result != "":
+        play_again()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            exit()
-
-        # Se o usuário pressionou `Enter`,
-        # lê a string digitada
+            sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RETURN:
-                for palavra in palavras:
-                    palavra = pygame.event.wait().text.strip()
-
-    # Limpa a tela
-    tela.fill(cor_fundo)
-
-    # Desenha os quadrados
-    for quadrado in quadrados:
-        pygame.draw.rect(tela, cor_rosa, quadrado)
-#        # Desenha o texto no meio do quadrado
-        # tela.blit(texto.render("A", True, (255, 255, 255)), (quadrado.centerx-8, quadrado.centery-10))
-        
-    # for palavra in palavras:
-    #     palavra = pygame.event.wait().text.strip()
-        
-    for palavra in palavras:
-        for i in palavra:
-            tela.blit(texto.render(i, True, (255, 255, 255)), (quadrado.centerx-8, quadrado.centery-10))
-
-    # Atualiza a tela
-    pygame.display.update()
-
-
-
-####################
-
-
-# import pygame
-
-# # Inicializa o Pygame
-# pygame.init()
-
-# # Cria a tela
-# tela = pygame.display.set_mode((640, 480))
-
-# # Define a cor de fundo
-# tela.fill((0, 0, 0))
-
-# # Cria o quadrado
-# quadrado = pygame.Rect((320, 240), (100, 100))
-
-# # Desenha o quadrado
-# pygame.draw.rect(tela, (255, 255, 255), quadrado)
-
-# # Cria o texto
-# texto = pygame.font.SysFont("Arial", 50)
-
-# # Desenha o texto no meio do quadrado
-# tela.blit(texto.render("A", True, (255, 0, 0)), (quadrado.centerx, quadrado.centery))
-
-# # Atualiza a tela
-# pygame.display.update()
-
-# # Loop principal
-# while True:
-#     for event in pygame.event.get():
-#         # Verifica se o usuário fechou a janela
-#         if event.type == pygame.QUIT:
-#             pygame.quit()
-#             sys.exit()
-
+                if game_result != "":
+                    reset()
+                else:
+                    if len(current_guess_string) == 5:
+                        check_guess(current_guess)
+            elif event.key == pygame.K_BACKSPACE:
+                if len(current_guess_string) > 0:
+                    delete_letter()
+            else:
+                key_pressed = event.unicode.upper()
+                if key_pressed in "QWERTYUIOPASDFGHJKLZXCVBNM" and key_pressed != "":
+                    if len(current_guess_string) < 5:
+                        create_new_letter()
